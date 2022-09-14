99de69489bf824a24abbfd281e9d39e5df84acd5
Gabriel Albuquerque Magalhães <gabriel.ashm@hotmail.com>
2022-09-13 21:46:17 -0300

    👌 IMPROVE: responsive Header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,16 +1,26 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Gabriel Magalhães</Text>
-        <Text color="gray.300" fontSize="small">
-          gabriel.ashm@hotmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Gabriel Magalhães</Text>
+          <Text color="gray.300" fontSize="small">
+            gabriel.ashm@hotmail.com
+          </Text>
+        </Box>
+      )}
 
-      <Avatar size="md" name="Gabriel Magalhães" src="https://github.com/magaliais.png" />
+      <Avatar
+        size="md"
+        name="Gabriel Magalhães"
+        src="https://github.com/magaliais.png"
+      />
     </Flex>
   );
 }
