efa3b2dee365403ded4a60ac24e5d539530d10c4
Gabriel Albuquerque Magalhães <gabriel.ashm@hotmail.com>
2022-09-21 12:21:59 -0300

    👌 IMPROVE: enabling react-query-devtools

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { ChakraProvider } from '@chakra-ui/react';
 import theme from '../styles/theme';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext';
 import { makeServer } from '../services/mirage';
@@ -21,6 +22,8 @@ function MyApp({ Component, pageProps }: AppProps) {
         <SidebarDrawerProvider>
           <Component {...pageProps} />
         </SidebarDrawerProvider>
+
+        <ReactQueryDevtools />
       </QueryClientProvider>
     </ChakraProvider>
   );}
