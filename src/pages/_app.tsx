fa07cef3df680d964cafc1ba0cdbd8fb2e1dd192
Gabriel Albuquerque Magalhães <gabriel.ashm@hotmail.com>
2022-09-21 20:50:37 -0300

    👌 IMPROVE: module for queryClient from react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,13 +7,12 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext';
 import { makeServer } from '../services/mirage';
+import { queryClient } from '../services/queryClient';
 
 // ? Inicia a fake API do mirage caso esteja em modo de desenvolvimento
 if(process.env.NODE_ENV === 'development') {
     makeServer();
 }
-
-const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
